4eb823835913e46ac271ee8bbf2710a4b3ea61d7
Dimitar Vashkov <dimitarvashkov@icloud.com>
2018-04-01 17:00:29 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
     resp.write(Time.now)
-    time = Time.now.hour.after?(12)
+    time = Time.now.hour > 12
     if time
       resp.write("Good Morning!")
     else
