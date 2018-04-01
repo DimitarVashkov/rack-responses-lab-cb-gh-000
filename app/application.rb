9626b42babcb73be18ed2b2f1ff2b83c9bbae19b
Dimitar Vashkov <dimitarvashkov@icloud.com>
2018-04-01 16:59:00 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,11 +5,11 @@ class Application
     resp.write(Time.now)
     time = Time.now.hour.after?(12)
     if time
-
+      resp.write("Good Morning!")
     else
-      
+      resp.write("Good Afternoon!")
     end
-    
+
 
     resp.finish
   end
